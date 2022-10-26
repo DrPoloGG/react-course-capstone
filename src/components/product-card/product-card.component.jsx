9641aa1a428bcd436980312d977888425fa8a28c
Polo <polo@Polos-MacBook-Pro.local>
2022-10-26 09:08:53 -0500

    added addToCart methods and cartItem components

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,9 +1,16 @@
+import { useContext } from "react";
+
+import { CartContext } from "../../contexts/cart.context";
+
 import Button from "../button/button.component";
 
 import './product-card.styles.scss';
 
 const ProductCard = ({ product }) => {
+    const { addItemToCart } = useContext(CartContext);
     const { name, price, imageUrl } = product;
+
+    const addProductToCart = () => addItemToCart(product);
 
     return (
         <div className="product-card-container">
@@ -12,7 +19,7 @@ const ProductCard = ({ product }) => {
                 <span className="name">{ name }</span>
                 <span className="price">{ price }</span>
             </div>
-            <Button buttonType='inverted'>ADD TO CART</Button>
+            <Button buttonType='inverted' onClick={addProductToCart}>ADD TO CART</Button>
         </div>
     )
 }
